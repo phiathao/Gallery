23fe093f23e4b7489c9bf1b15ba56e1b8e8c2032
phiathao <phiat11@gmail.com>
2018-12-02 16:51:39 -0600

    key to id

--- src/components/GarlleryItem/GalleryItem.jsx
+++ src/components/GarlleryItem/GalleryItem.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class GalleryItem extends Component {
     render(){
         return (
-            <li key={this.props.key}>
+            <li key={this.props.id}>
                 <img alt={this.props.alt} src={this.props.img}/>
-                <button onClick={ ()=> this.props.likeFn(this.props.key)}>{this.props.like}</button>
+                <button onClick={ ()=> this.props.likeFn(this.props.id)}>{this.props.like}</button>
             </li>
         );
     }
