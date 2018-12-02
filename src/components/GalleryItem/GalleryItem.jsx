5d2cc0adbdf50cfda4b496a3e5844264db166d4f
phiathao <phiat11@gmail.com>
2018-12-02 17:20:20 -0600

    remove key

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class GalleryItem extends Component {
     render(){
         return (
-            <li key={this.props.id}>
+            <div>
                 <img alt={this.props.alt} src={this.props.img}/>
                 <button onClick={ ()=> this.props.likeFn(this.props.id)}>{this.props.like}</button>
-            </li>
+            </div>
         );
     }
 }
