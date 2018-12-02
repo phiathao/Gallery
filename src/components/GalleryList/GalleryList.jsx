6c97d73e1ddc6b1b12319e64b95f78b4d785ee26
phiathao <phiat11@gmail.com>
2018-12-02 16:50:25 -0600

    gallery item

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem'
 
 class GalleryList extends Component {
     render(){
         let GalleryList = this.props.GalleryList.map( (gallery) => {
-            return <li key={gallery.id}><img alt='goat' src={gallery.path}/></li>
+            return <GalleryItem img={gallery.url}/>
         })
         return (
             <div>
