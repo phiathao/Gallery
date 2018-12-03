4019a61ca094ffea2cbfa47882045cdfcfe1a9fd
phiathao <phiat11@gmail.com>
2018-12-02 19:35:06 -0600

    add state to item

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+    state = {
+        pictureIsClick : false
+    }
     render(){
         return (
             <div>
                 <img alt={this.props.alt} src={this.props.img}/>
-                <button onClick={ ()=> this.props.likeFn(this.props.id)}>{this.props.like}</button>
+                <button onClick={ ()=> this.props.likeFn(this.props.id)}>{this.props.like} Likes</button>
             </div>
         );
     }
