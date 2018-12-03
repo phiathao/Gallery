dd78124e5e51e5dd43d1c19f2457c34521172522
phiathao <phiat11@gmail.com>
2018-12-02 22:51:54 -0600

    add description

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,13 +1,13 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/1.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/2.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/3.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 5, path: 'images/4.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 6, path: 'images/5.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 7, path: 'images/6.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 8, path: 'images/7.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 9, path: 'images/8.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 2, path: 'images/1.jpg', description: '1994 Refugee Camp.', likes: 0 },
+    { id: 3, path: 'images/2.jpg', description: '2016 Alcatraz.', likes: 0 },
+    { id: 4, path: 'images/3.jpg', description: 'My brother small little dog', likes: 0 },
+    { id: 5, path: 'images/4.jpg', description: '2011 graduation with my sister.', likes: 0 },
+    { id: 6, path: 'images/5.jpg', description: 'food, food, food', likes: 0 },
+    { id: 7, path: 'images/6.jpg', description: 'Niece is pondering what to do', likes: 0 },
+    { id: 8, path: 'images/7.jpg', description: 'Niece having a taste of lemon', likes: 0 },
+    { id: 9, path: 'images/8.jpg', description: 'food, food, and more food', likes: 0 }
 ];
 
 module.exports = galleryItems;
